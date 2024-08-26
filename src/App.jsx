19496c02e389076bb99afc6e2dc09a3f14c501d7
mahesh <maheshbemanapalli@gmail.com>
2024-08-26 19:45:55 +0530

    added the data rendering dynamically when the buttons are clicked using useState hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
+import { useState } from 'react';
+import { EXAMPLES } from './data.js';
 import Header from './components/Header.jsx'
 import TabButton from './components/TabButton.jsx'
 
 function App() {
-  function handleClick() {
-    console.log(`Button clicked: from APP`);
+  //useState returns an array with two elements, the first element is the current state value and the second element is a function that allows us to update the state value
+  const [selectedTopic, selectedButtonText] = useState('');
+  function handleClick(selectedButton) {
+    selectedButtonText(selectedButton)
+    // console.log(selectedText);
+  }
+  let tabContent = <p>Click on a tab to see an example!</p>;
+  if(selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
-     
-      <section id='examples'>
-        <h2>Examples</h2>
-        <menu>
-        <TabButton onSelect={handleClick}>Component</TabButton>
-        <TabButton onSelect={handleClick}>JSX</TabButton>
-        <TabButton onSelect={handleClick}>Props</TabButton>
-        <TabButton onSelect={handleClick}>State</TabButton>
-        </menu>
-      </section>
+
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            {/* we can use arrow function to pass the value to the function*/}
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleClick('components')}>Component</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleClick('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleClick('state')}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
-    
+
   );
 }
 
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,9 @@
 //we can use children prop to render the content of the TabButton component
 //we can also use prop keyword and give it in label , but this is also another approach to it
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect,isSelected }) {
   return (
     <li>
-        <button onClick={onSelect}>{children}</button>
-        
+        <button className={isSelected ? "active" : ""} onClick={onSelect}>{children}</button>
     </li>
   );
 }
